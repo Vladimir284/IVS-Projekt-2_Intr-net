1b3592fb935c8a2ff7265369133c33866b9f23ec
Vladimir <xmecia00@stud.fit.vutbr.cz>
2021-03-23 17:32:26 +0100

    Pridana trieda Calculator podla grafu plan.png

---
+++ Calc/main.py
@@ -0,0 +1,21 @@
+class Calculator:
+
+    def __init__(self):
+        _Operand = 0.0
+        _ID_operation = 0
+        _Last_answer = 0
+
+    def getMemory(self):
+        pass
+
+    def setMemory(self):
+        pass
+
+    def getOperand(self):
+        pass
+
+    def setOperand(self):
+        pass
+
+    def executeOpration(self, ID_Operation):
+        pass
