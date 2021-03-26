10865af7e6c0c83d2a6c67193b2dcd51acbc7f88
Vladimir <xmecia00@stud.fit.vutbr.cz>
2021-03-26 15:56:18 +0100

    Vytvorena trieda calculator podla UML navrhu

---
+++ src/main.py
@@ -0,0 +1,56 @@
+import math_lib
+import memory_operation
+
+
+class Calculator:
+    '''
+    Stores values of memory and contains operands methods
+    '''
+
+    def __init__(self):
+        '''
+        Constructor
+        Every parameter will be set to zero
+        '''
+        _Operand = 0.0
+        _ID_operation = 0
+        _Last_answer = 0
+
+    def getMemory(self):
+        '''
+        Return values currently stored on memory
+        :return:
+        '''
+        pass
+
+    def setMemory(self):
+        '''
+        Set memory on current value
+        :return:
+        '''
+        pass
+
+    def getOperand(self):
+        '''
+        Read from In.
+        :return:
+        '''
+        pass
+
+    def setOperand(self):
+        '''
+        Set ID of next operation depending on opration entered
+        :return:
+        '''
+        pass
+
+    def executeOpration(self, ID_Operation):
+        '''
+        Executes operation depending on param.
+        :param ID_Operation:
+        :return:
+        '''
+        pass
+
+# TO-DO
+# Enumeration of operations
