b0b14101c3783e02e4797681ad0db5a636a731b7
MatejS99 <m.sauer.cz@gmail.com>
2021-03-30 16:23:55 +0200

    gui buttons are displaying

--- src/calc.py
+++ src/calc.py
@@ -12,54 +12,36 @@ display = tk.Entry(calc, width=30)
 display.grid(row=0, column=0, columnspan=5, sticky="nesw")
 display.config(justify="right")
 
-num_0 = tk.Button(calc, text="0")
 
-num_1 = tk.Button(calc, text="1")
+def string_button_click(number):
+    display.insert("insert", number)
 
-num_2 = tk.Button(calc, text="2")
 
-num_3 = tk.Button(calc, text="3")
-
-num_4 = tk.Button(calc, text="4")
-
-num_5 = tk.Button(calc, text="5")
-
-num_6 = tk.Button(calc, text="6")
-
-num_7 = tk.Button(calc, text="7")
-
-num_8 = tk.Button(calc, text="8")
-
-num_9 = tk.Button(calc, text="9")
-
+# buttons def
+num_0 = tk.Button(calc, text="0", command=lambda: string_button_click("0"))
+num_1 = tk.Button(calc, text="1", command=lambda: string_button_click("1"))
+num_2 = tk.Button(calc, text="2", command=lambda: string_button_click("2"))
+num_3 = tk.Button(calc, text="3", command=lambda: string_button_click("3"))
+num_4 = tk.Button(calc, text="4", command=lambda: string_button_click("4"))
+num_5 = tk.Button(calc, text="5", command=lambda: string_button_click("5"))
+num_6 = tk.Button(calc, text="6", command=lambda: string_button_click("6"))
+num_7 = tk.Button(calc, text="7", command=lambda: string_button_click("7"))
+num_8 = tk.Button(calc, text="8", command=lambda: string_button_click("8"))
+num_9 = tk.Button(calc, text="9", command=lambda: string_button_click("9"))
 n_root = tk.Button(calc, text="x^1/n")
-
 n_power = tk.Button(calc, text="x^n")
-
 absolute_value = tk.Button(calc, text="|x|")
-
 factorial = tk.Button(calc, text="x!")
-
 all_clear = tk.Button(calc, text="AC")
-
 clear = tk.Button(calc, text="C")
-
-l_bracket = tk.Button(calc, text="(")
-
-r_bracket = tk.Button(calc, text=")")
-
-plus = tk.Button(calc, text="+")
-
-minus = tk.Button(calc, text="-")
-
-times = tk.Button(calc, text="*")
-
-divide = tk.Button(calc, text="/")
-
-result = tk.Button(calc, text="=")
-
-dot = tk.Button(calc, text=".")
-
+l_bracket = tk.Button(calc, text="(", command=lambda: string_button_click("("))
+r_bracket = tk.Button(calc, text=")", command=lambda: string_button_click(")"))
+plus = tk.Button(calc, text="+", command=lambda: string_button_click("+"))
+minus = tk.Button(calc, text="-", command=lambda: string_button_click("-"))
+times = tk.Button(calc, text="*", command=lambda: string_button_click("*"))
+divide = tk.Button(calc, text="/", command=lambda: string_button_click("/"))
+result = tk.Button(calc, text="=",)
+dot = tk.Button(calc, text=".", command=lambda: string_button_click("."))
 ans = tk.Button(calc, text="ANS")
 
 buttons_array = [l_bracket, r_bracket, n_power, n_root, all_clear, num_7, num_8, num_9, divide, clear, num_4, num_5,
