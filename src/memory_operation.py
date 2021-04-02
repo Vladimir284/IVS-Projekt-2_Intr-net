186834c027fef31482890a29b139b9906c4aa992
Vladimir <xmecia00@stud.fit.vutbr.cz>
2021-04-02 09:31:13 +0200

    Implementovana kostra modulu memory_operation

---
+++ src/memory_operation.py
@@ -0,0 +1,31 @@
+'''@package
+
+    Contains operations with memory
+'''
+
+
+def memory_Recall():
+    pass
+
+
+def display_clear():
+    ''' Clears the display but will not delete last saved answer '''
+    pass
+
+
+def ans():
+    # TODO Discuss
+    # Discus what the function is intended to do
+    pass
+
+
+def memory_clear():
+    ''' Clears display and deletes last saved answer'''
+    pass
+
+
+def hint():
+    # TODO: Discus
+    # Defined by UML this was supposed to be class since it has two methods
+    # Should all memory operations be classes ? Or make two separet methods in one module ?
+    pass
