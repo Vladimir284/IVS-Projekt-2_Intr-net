580f47d14cea8940cd1185fc86f78c812ee19914
Vladimir <xmecia00@stud.fit.vutbr.cz>
2021-04-13 19:04:04 +0200

    Added documentation for profiling.py

--- src/profiling.py
+++ src/profiling.py
@@ -1,21 +1,46 @@
 ## @package profiling
 # Module for computing standard deviation
+# Inside varriables that store number are initilaized to zero
 # @note \f$ s = \sqrt{\frac{1}{N-1} \cdot (\sum_{i=1}^{N} x_{i}^2-N \cdot \overline{x}^2})\f$
-# @param numbers Sequence of numbers from file or stdin
+# @param numbers Sequence of numbers from stdin
 
 import math_lib
 
 # Read and split
-input_string = input() #!<  Input string of numbers separated by empty space
+##
+# Input string with numbers separated by empty space
+input_string = input()
+##
+# Separated numbers from #input_string stored in array
 array_of_numbers = input_string.split(" ")
 
 # Declare all used varriable
+##
+# Amount of numbers in #array_of_numbers
 amount_of_numbers = len(array_of_numbers)
+
+##
+# Sum of all numbers in #array_of_numbers
 sum_of_numbers = 0.0
-average_of_numbers = 0.0
+
+##
+# Mean of all the numbers in #array_of_numbers
+mean_of_numbers = 0.0
+
+##
+# Sum of \f$ x_{i}\f$ squared and #mean_of_number squared
 sum_of_squared_difference = 0.0
+
+##
+# Final value
 standard_deviation = 0.0
+
+##
+# Difference of \f$ x_{i} \f$ and \f$ \overline{x}\f$
 difference_of_element = 0.0
+
+##
+# Square of #difference_of_element
 square_of_difference_of_element = 0.0
 variance = 0.0
 
@@ -24,13 +49,13 @@ for number_string in array_of_numbers:
     if number_string.isdigit():
         sum_of_numbers = math_lib.summary(float(number_string), sum_of_numbers)
 
-# Average of elements
-average_of_numbers = math_lib.division(sum_of_numbers, amount_of_numbers)
+# Mean of elements
+mean_of_numbers = math_lib.division(sum_of_numbers, amount_of_numbers)
 
 # Summary of difference of each element with average
 for number_string in array_of_numbers:
     if number_string.isdigit():
-        difference_of_element = math_lib.subtraction(float(number_string), average_of_numbers)
+        difference_of_element = math_lib.subtraction(float(number_string), mean_of_numbers)
         square_of_difference_of_element = math_lib.nth_power(difference_of_element, 2)
         sum_of_squared_difference = math_lib.summary(sum_of_squared_difference, square_of_difference_of_element)
 
