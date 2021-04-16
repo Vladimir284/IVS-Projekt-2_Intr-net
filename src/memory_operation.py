26f226473fcb7a2e42bd5e6acb5928ebb2f36629
Vladimir <xmecia00@stud.fit.vutbr.cz>
2021-04-16 15:48:06 +0200

    Documentation of memory_operations

--- src/memory_operation.py
+++ src/memory_operation.py
@@ -1,36 +1,30 @@
-## @file memory_operation.py
-# @brief Module with implementation of operations with memory
+## @package memory_operation
+# @brief Package with implementation of operations with memory
 #
 # All of the functions are void and don't take any paramater
+#
+# @file memory_operation.py
+# @brief Module with implementation of operations with memory
+#
 
 import calc
 
 
 ## Clears the display
-## TO-DO Again, what function are we talking about and also
-# @note TO-DO If you have something to add
-# @param TO-DO After @param, type name of parameter and after it what it represents, For each param youll have one line
-# @return TO-DO What this function retuns
+# @note Does not clear the memory
 def display_clear():
     calc.clear()
     pass
 
 
 ## Clears the display and the last saved answer (ANS)
-## TO-DO Again, what function are we talking about and also
-# @note TO-DO If you have something to add
-# @param TO-DO After @param, type name of parameter and after it what it represents, For each param youll have one line
-# @return TO-DO What this function retuns
 def memory_clear():
     calc.c.setMemory(calc.c, 0)
     pass
 
 
 ## Opens manual for the user
-## TO-DO Again, what function are we talking about and also
-# @note TO-DO If you have something to add
-# @param TO-DO After @param, type name of parameter and after it what it represents, For each param youll have one line
-# @return TO-DO What this function retuns
+# @note opens manual.pdf
 def hint_show_man():
     # TO-DO write manual
     pass
